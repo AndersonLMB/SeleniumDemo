d314dee3707d49bc3b94ab68d0ef397463b52a40
lmb <1603497362@qq.com>
2018-11-21 16:30:42 +0800

    commit

--- Selenium/Program.cs
+++ Selenium/Program.cs
@@ -4,11 +4,17 @@ using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace Selenium
+namespace SeleniumDemo
 {
     public class Program
     {
         public static void Main(string[] args)
+        {
+            DeleteThemes();
+            Console.ReadLine();
+        }
+
+        public static void DebindThemes()
         {
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("--start-maximized");
@@ -27,12 +33,50 @@ namespace Selenium
                 Task.Delay(1000).Wait();
                 var leftArrowElement = webDriver.FindElement(By.CssSelector(".config2organize .icon-triangle-left"));
                 leftArrowElement.Click();
-
                 var confirmDialog = webDriver.SwitchTo().Alert();
                 confirmDialog.Accept();
                 Task.Delay(1000).Wait();
             });
             Console.ReadLine();
         }
+
+        public static void DeleteThemes()
+        {
+            ChromeOptions options = new ChromeOptions();
+            IWebDriver webDriver = new ChromeDriver(options);
+            webDriver.Navigate().GoToUrl("http://localhost/ZJG.GisClient/default.aspx?f=mm");
+            webDriver.Manage().Timeouts().ImplicitWait = new TimeSpan(10000 * 50000);
+            IWebElement element = webDriver.FindElement(By.CssSelector("[data-navid=\"manege_config\"]"));
+            //checkbox_false_full
+            //subject-config 
+            element.Click();
+
+
+
+
+            var elements = webDriver.FindElements(By.CssSelector(".subject-config .checkbox_false_full"));
+            elements.ToList().ForEach((checkbox) =>
+            {
+                checkbox.Click();
+                Task.Delay(500).Wait();
+            });
+            //icon-wh-trash
+
+            IWebElement trashBinElement = webDriver.FindElement(By.CssSelector(".subject-config .icon-wh-trash"));
+
+
+            trashBinElement.Click();
+
+
+            var confirmDialog = webDriver.SwitchTo().Alert();
+            Task.Delay(200).Wait();
+            confirmDialog.Accept();
+
+
+
+
+
+        }
+
     }
 }
